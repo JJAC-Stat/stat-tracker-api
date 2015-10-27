13b329f920501947ec3d72d6a8eb82516ceac1f6
apierce1992 <apierce1992@gmail.com>
2015-10-26 23:51:35 -0400

    fixed local deploy

---
+++ stat_tracker/stat_tracker/wsgi.py
@@ -0,0 +1,22 @@
+"""
+WSGI config for stat_tracker project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/1.8/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+from whitenoise.django import DjangoWhiteNoise
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "stat_tracker.settings")
+
+application = DjangoWhiteNoise(get_wsgi_application())
+application = DjangoWhiteNoise(application)
+
+from dj_static import Cling
+
+application = Cling(get_wsgi_application())
