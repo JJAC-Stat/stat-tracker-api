d6b989bcb3be02debfa7e7093ce1a6bb7fdc84dc
apierce1992 <apierce1992@gmail.com>
2015-10-27 17:19:25 -0400

    commented out some helper functions views

--- stat_tracker/api/views.py
+++ stat_tracker/api/views.py
@@ -13,21 +13,30 @@ class ActivityViewSet(viewsets.ModelViewSet):
     def perform_create(self, serializer):
         serializer.save(user=self.request.user)
 
+    # def get_queryset(self):
+    #     activity_id = self.kwargs['activity_pk']
+    #     get_object_or_404(Activity, pk=activity_id)
+    #     return self.queryset.filter(activity_id=activity_id)
+    #
+    # def get_serializer_context(self):
+    #     context = super().get_serializer_context().copy()
+    #     context['activity_pk'] = self.kwargs['activity_pk']
+    #     return context
 
 class DataPointViewSet(viewsets.ModelViewSet):
     queryset = DataPoint.objects.all()
     serializer_class = DataPointSerializer
 
-    def get_queryset(self):
-        activity_id = self.kwargs['activity_id']
-        get_object_or_404(Activity, pk=activity_id)
-        return self.queryset.filter(activity_id=activity_id)
-
-    def get_serializer_context(self):
-        context = super().get_serializer_context().copy()
-        context['activity_id'] = self.kwargs['activity_id']
-        return context
-        # return {'question_pk': self.kwargs['question_pk']}
+    # def get_queryset(self):
+    #     activity_id = self.kwargs['activity_id']
+    #     get_object_or_404(Activity, pk=activity_id)
+    #     return self.queryset.filter(activity_id=activity_id)
+    #
+    # def get_serializer_context(self):
+    #     context = super().get_serializer_context().copy()
+    #     context['activity_id'] = self.kwargs['activity_id']
+    #     return context
+    #     # return {'question_pk': self.kwargs['question_pk']}
 
     def perform_create(self, serializer):
         serializer.save(user=self.request.user)
