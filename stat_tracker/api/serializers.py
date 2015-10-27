0b244cb9a621cefebfac4a26ec8da7695b909d9d
Jermaine Kee <godson919@icloud.com>
2015-10-27 12:32:11 -0400

    reset

--- stat_tracker/api/serializers.py
+++ stat_tracker/api/serializers.py
@@ -5,8 +5,3 @@ class ActivitySerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Activity
         fields = ('id', 'title', 'timestamp', 'datapoints')
-<<<<<<< HEAD
-
-
-=======
->>>>>>> 7f99ef66c1d8e03a64c96c83d3eb364fbdc71e4e
