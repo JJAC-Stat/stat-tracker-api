c6e374792816743362a253e8d8280b20dcc3b6ec
apierce1992 <apierce1992@gmail.com>
2015-10-27 16:44:53 -0400

    tweaked allowed hosts

--- stat_tracker/stat_tracker/settings.py
+++ stat_tracker/stat_tracker/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'pos3r8fdg0xqroclf5macie376qojzur86xw3s8wpupntdufjm'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
